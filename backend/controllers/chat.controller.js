59e5de5d70a1a65aebc6764a51f1f9c1c207fc37
ahviky02 <cpaavdhesh@gmail.com>
2025-01-28 01:23:28 +0530

    menu icon add

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -1,13 +1,16 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 
 export const getChatUsers = async (req, res) => {
-     console.log("Get chat users");
   try {
-    const users = await User.find({ _id: { $ne: req.user._id } });
+    // Fetch all users from the User model
+    const users = await User.findAll;
+    // Respond with the list of users in JSON format
     res.status(200).json(users);
-    console.log("Get chat users success");
+    // Log the users in the console
+    console.log(JSON.stringify(users));
   } catch (error) {
-    console.error("Get chat users error:", error);
-    res.status(500).json({ message: "Internal server error" });
+    // Log and respond with an error message in case of failure
+    console.error('Get chat users error:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
