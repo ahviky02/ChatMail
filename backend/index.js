c41b8a8d1a81a40de5be8abf0b3033cfb5578e42
ahviky02 <cpaavdhesh@gmail.com>
2025-02-10 01:03:34 +0530

    test deploy

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-
+import cors from 'cors'; // Import the cors package
 import authRoutes from './routes/auth.routes.js';
 import chatRoutes from './routes/chat.routes.js';
-import mailRoutes from './routes/mail.routes.js';
+import mailRoutes from './routes/mail.routes.js'
 import connectToMongoDB from './lib/db.js';
-import { app, server } from './lib/socket.js';
+import cookieParser from 'cookie-parser';
+import {app,server} from './lib/socket.js';
+
+import path from 'path';
+
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(cookieParser());
@@ -20,20 +21,22 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-}));
+})); // Enable CORS for all routes
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/mail", mailRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
   });
 }
 
