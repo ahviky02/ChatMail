f0a3ecc086119add7497b9d1fa399ffb1c8e8c98
ahviky02 <cpaavdhesh@gmail.com>
2025-02-09 23:47:01 +0530

    test deploy

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,13 @@ import connectToMongoDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import {app,server} from './lib/socket.js';
 
+import path from 'path';
+
 
 dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(cors({
@@ -21,11 +26,19 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/mail", mailRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   connectToMongoDB();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: import.meta.env.MODE=== "development"? "http://localhost:8001/api":"/api",
      withCredentials: true,
 });
 
